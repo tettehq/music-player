49d20ef63260a8292202611d7385ea3bca1ccba4
tettehq <ronyquash@gmail.com>
2025-04-12 23:18:21 +0000

    number 4

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let ctrlIcon = document.getElementById("ctrlIcon");
 let songTitle = document.getElementById("songTitle");
 let coverImg = document.getElementById("cover");
 let artistes = document.getElementById("artistes");
+
 let playlist = [
     {
         title: "Kilimanjaro",
@@ -29,113 +30,104 @@ let playlist = [
         image: "./media/hgf_thumb.png",
         source: "./media/omah_lay_holy_ghost_official_visualizer_mp3_74633.mp3"
     }
-]
-// const playlist1 = ["/media/tiwa_savage_black_sherif_young_jonn_kilimanjaro_official_lyric_video_mp3_66142.mp3", "/media/joeboy_ft._qing_madi_adenuga_official_music_video_mp3_57631.mp3"]
+];
 
-function loadSongData() {
-    song.innerHTML = `<source id="source" src=${playlist[0].source} type="audio/mpeg">`;
-    songTitle.innerText = playlist[0].title;
-    coverImg.src = playlist[0].image;
-    artistes.innerText = playlist[0].artistes;
-    playPause();
-}
+let currentIndex = 0;
 
-document.addEventListener('load', loadSongData());
+document.addEventListener("DOMContentLoaded", () => {
+    loadSongData(currentIndex);
+});
 
 song.onloadedmetadata = function () {
     progress.max = song.duration;
     progress.value = song.currentTime;
+};
+
+function updateSongUI(index) {
+    song.innerHTML = `<source id="source" src="${playlist[index].source}" type="audio/mpeg">`;
+    songTitle.innerText = playlist[index].title;
+    coverImg.src = playlist[index].image;
+    artistes.innerText = playlist[index].artistes;
+}
+
+async function loadSongData(index) {
+    currentIndex = index;
+    updateSongUI(index);
+    song.load();
+
+    try {
+        await song.play();
+        ctrlIcon.classList.remove("fa-play");
+        ctrlIcon.classList.add("fa-pause");
+    } catch (err) {
+        console.error("Playback error:", err);
+    }
 }
 
 function playPause() {
-    if (ctrlIcon.classList.contains("fa-pause") && song.play()) {
+    if (ctrlIcon.classList.contains("fa-pause")) {
         song.pause();
         ctrlIcon.classList.remove("fa-pause");
         ctrlIcon.classList.add("fa-play");
-    }
-    else {
-        song.play();
-        ctrlIcon.classList.remove("fa-play");
-        ctrlIcon.classList.add("fa-pause");
+    } else {
+        song.play().then(() => {
+            ctrlIcon.classList.remove("fa-play");
+            ctrlIcon.classList.add("fa-pause");
+        }).catch(err => {
+            console.error("Play error:", err);
+        });
     }
 }
-
-if (song.play()) {
-    setInterval(() => {
-        progress.value = song.currentTime;
-    },500)
-}
-
-let loop = false;
-let interval = parseInt(song.duration * 1000);;
-console.log(typeof interval);
 
 setInterval(() => {
-    if (!loop && song.currentTime == song.duration) {
+    progress.value = song.currentTime;
+}, 500);
+
+let loop = false;
+
+setInterval(() => {
+    if (!loop && song.currentTime >= song.duration) {
         nextSong();
-    }
-    else {
+    } else if (loop) {
         loopSong();
     }
 }, 1000);
 
-progress.onchange = function() {
-    song.play();
+progress.onchange = function () {
     song.currentTime = progress.value;
-    ctrlIcon.classList.remove("fa-play");
-    ctrlIcon.classList.add("fa-pause");
+    song.play().then(() => {
+        ctrlIcon.classList.remove("fa-play");
+        ctrlIcon.classList.add("fa-pause");
+    }).catch(err => console.error("Seek play error:", err));
+};
 
-    console.log("changed")
+async function nextSong() {
+    currentIndex = (currentIndex + 1) % playlist.length;
+    updateSongUI(currentIndex);
+    song.load();
+
+    try {
+        await song.play();
+    } catch (err) {
+        console.error("Next song play error:", err);
+    }
 }
 
-function nextSong() {
-    song.currentTime = 0;
-    let source = document.getElementById("source").src;
-    for (let i = 0; i < playlist.length; i++ ) {
-        if (source.includes(playlist[i].source) == true && i + 1 < playlist.length) {
-            song.innerHTML = `<source id="source" src=${playlist[i + 1].source} type="audio/mpeg">`
-            songTitle.innerText = playlist[i + 1].title;
-            coverImg.src = playlist[i + 1].image;
-            artistes.innerText = playlist[i + 1].artistes;
-            break;
-        }
-        else {
-            song.innerHTML = `<source id="source" src=${playlist[0].source} type="audio/mpeg">`;
-            songTitle.innerText = playlist[0].title;
-            coverImg.src = playlist[0].image;
-            artistes.innerText = playlist[0].artistes;
-        }
-    }
+async function prevSong() {
+    currentIndex = (currentIndex - 1 + playlist.length) % playlist.length;
+    updateSongUI(currentIndex);
     song.load();
-    song.play();
-}
 
-function prevSong() {
-    song.currentTime = 0;
-    let lastIndex = playlist.length - 1
-    let source = document.getElementById("source").src;
-    for (let i = lastIndex; i > -1; i-- ) {
-        if (source.includes(playlist[i].source) == true && i - 1 > -1) {
-            song.innerHTML = `<source id="source" src=${playlist[i - 1].source} type="audio/mpeg">`
-            songTitle.innerText = playlist[i - 1].title;
-            coverImg.src = playlist[i - 1].image;
-            artistes.innerText = playlist[i - 1].artistes;
-            break;
-        }
-        else {
-            song.innerHTML = `<source id="source" src=${playlist[i].source} type="audio/mpeg">`;
-            songTitle.innerText = playlist[i].title;
-            coverImg.src = playlist[i].image;
-            artistes.innerText = playlist[i].artistes;
-        }
+    try {
+        await song.play();
+    } catch (err) {
+        console.error("Previous song play error:", err);
     }
-    song.load();
-    song.play();
 }
 
 function loopSong() {
-    if (song.currentTime == song.duration) {
+    if (song.currentTime >= song.duration) {
         song.currentTime = 0;
-        song.play();
+        song.play().catch(err => console.error("Loop play error:", err));
     }
 }
